a6ea27a0ef9849c4a6fde555c4081ed937a36fcf
Christian Saunders <cps@kilobit.ca>
2019-12-13 16:58:00 -0700

    Added a deep version of Expect.

--- assert/assert.go
+++ assert/assert.go
@@ -3,10 +3,18 @@
 package assert // import "kilobit.ca/go/tested/assert"
 
 import "testing"
+import "reflect"
 
 func Expect(t *testing.T, expected interface{}, actual interface{}) {
 
 	if expected != actual {
+		t.Errorf("Expected %v, Got %v", expected, actual)
+	}
+}
+
+func ExpectDeep(t *testing.T, expected interface{}, actual interface{}) {
+
+	if ! reflect.DeepEqual(expected, actual) {
 		t.Errorf("Expected %v, Got %v", expected, actual)
 	}
 }
--- assert/assert_test.go
+++ assert/assert_test.go
@@ -8,3 +8,19 @@ func TestAssertTest(t *testing.T) {
 
 	Expect(t, true, true)
 }
+
+func TestExpectDeep(t *testing.T) {
+
+	exp := map[string]int{"foo": 1, "bar": 42}
+	act := map[string]int{"foo": 1, "bar": 42}
+
+	ExpectDeep(t, exp, act)
+}
+
+func TestExpectDeepNested(t *testing.T) {
+
+	exp := map[string]interface{}{"foo": 1, "bar": map[string]bool{"nested": true}}
+	act := map[string]interface{}{"foo": 1, "bar": map[string]bool{"nested": true}}
+
+	ExpectDeep(t, exp, act)
+}
